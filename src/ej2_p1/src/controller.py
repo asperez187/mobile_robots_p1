b5f1ec49a55c948664b99174c0695c24d223435b
nestoregon <nestormorales1998@gmail.com>
2020-02-19 12:40:31 +0100

    Changes

--- src/ej2_p1/src/controller.py
+++ src/ej2_p1/src/controller.py
@@ -3,8 +3,9 @@
 
 import sys
 import rospy
+import numpy as np
+import random
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
 
@@ -13,22 +14,40 @@ class Robot_bumper():
     def __init__(self, robot_name):
         self.robot_name = robot_name
         self.vel = Twist()
-        self.sub_scan = rospy.Subscriber(self.robot_name+"/scan", LaserScan, callback_laser)
+        self.scan = LaserScan()
+        self.sub_scan = rospy.Subscriber(self.robot_name+"/scan", LaserScan, self.callback_laser)
         self.pub_cmd = rospy.Publisher(self.robot_name+"/cmd_vel", Twist, queue_size=5)
+        self.rate = rospy.Rate(10) # 0.1 seconds sleep
 
     def callback_laser(self, msg):
-        self.scan = msg.data
+        """ Callback laser """
+        self.scan = np.array(msg.ranges)
+        # self.bumper = msg.ranges[:, 120]
+        self.bumper_left = np.mean(self.scan[:119])
+        self.bumper_middle = np.mean(self.scan[120 : 239])
+        self.bumper_right = np.mean(self.scan[240:])
+
+    def vel_straight(self):
+        """ Go straight """
+        self.vel.linear.x = 0.3
+        self.vel.angular.z = 0.0
+        self.pub_cmd.publish(self.vel)
+    
 
     def control(self):
+        """ hola """
+        # while not rospy.is_shutdown():
+
 
 def main():
     """Main function"""
     robot_class = Robot_bumper(str(sys.argv[1]))
+    robot_class.vel_straight()
     rospy.spin()
 
 if __name__ == "__main__":
     # initialize node
-    rospy.init_node('', anonymous=True)
+    rospy.init_node('controller', anonymous=True)
     try:
         main()
     except rospy.ROSInterruptException:
