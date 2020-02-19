8f0706d2891e5595452dace693e22fc37b7ad274
nestoregon <nestormorales1998@gmail.com>
2020-02-19 11:34:54 +0100

    Commiting changes

--- src/ej2_p1/src/controller.py
+++ src/ej2_p1/src/controller.py
@@ -5,18 +5,32 @@ import sys
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from sensor_msgs import LaserScan
-from std_msgs.msg import Int8
+from sensor_msgs.msg import LaserScan
+
+
+class Robot_bumper():
+    """ Class of the control """
+    def __init__(self, robot_name):
+        self.robot_name = robot_name
+        self.vel = Twist()
+        self.sub_scan = rospy.Subscriber(self.robot_name+"/scan", LaserScan, callback_laser)
+        self.pub_cmd = rospy.Publisher(self.robot_name+"/cmd_vel", Twist, queue_size=5)
+
+    def callback_laser(self, msg):
+        self.scan = msg.data
+
+    def control(self):
 
 def main():
-  """Main function"""
-  rospy.spin()
+    """Main function"""
+    robot_class = Robot_bumper(str(sys.argv[1]))
+    rospy.spin()
 
 if __name__ == "__main__":
-  # initialize node
-  rospy.init_node('', anonymous=True)
-  try:
-    main()
-  except rospy.ROSInterruptException:
-    print("Shutting down publisher")
-    pass
+    # initialize node
+    rospy.init_node('', anonymous=True)
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        print("Shutting down publisher")
+        pass
